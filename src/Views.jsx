7fb9fdb6ca41f0012895a3856b36a6d5f92ed1e0
dj-mc <49037253+dj-mc@users.noreply.github.com>
2022-11-18 17:33:40 -0600

    Protect relevant routes

--- src/Views.jsx
+++ src/Views.jsx
@@ -1,44 +1,44 @@
 import { Outlet, Route, Routes } from "react-router-dom";
 
+import Account from "./Account";
 import Article from "./Article";
 import NotFound from "./NotFound";
+import Protected from "./Protected";
 import logo from "./logo.svg";
 
 function MyComponent() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-    </div>
-  );
+  return <img src={logo} className="App-logo" alt="logo" />;
 }
 
 function Views() {
   return (
-    <>
-      <Routes>
-        <Route index element={<h1>Index</h1>} />
-        <Route path="/about" element={<h1>About</h1>} />
+    <Routes>
+      <Route index element={<h1>Index</h1>} />
+      <Route path="/about" element={<h1>About</h1>} />
+
+      <Route element={<Protected />}>
         <Route path="/home" element={<MyComponent />} />
+        <Route path="/account" element={<Account />} />
+      </Route>
 
-        <Route
-          path="/article"
-          element={
-            <>
-              Article content:
-              <Outlet />
-            </>
-          }
-        >
-          <Route index element={<h1>Articles</h1>} />
-          <Route path=":id" element={<Article />} />
-          <Route path="*" element={<p>Oops, no article found...</p>} />
-        </Route>
+      <Route
+        path="/article"
+        element={
+          <div>
+            <hr></hr>
+            Article content:
+            <Outlet />
+            <hr></hr>
+          </div>
+        }
+      >
+        <Route index element={<h1>Articles</h1>} />
+        <Route path=":id" element={<Article />} />
+        <Route path="*" element={<p>Oops, no article found...</p>} />
+      </Route>
 
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </>
+      <Route path="*" element={<NotFound />} />
+    </Routes>
   );
 }
 
